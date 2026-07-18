814fa70c375e89f948eb95e023a9951dfe4da095
thendguy <livm04@hotmail.com>
2026-07-18 02:14:44 +0100

    Sorted files

--- nob.c
+++ nob.c
@@ -1,14 +1,15 @@
-#define NOB_IMPLEMENTATION
 
-#include ".include/nob.h"
+
+#define NOB_IMPLEMENTATION
+#include "./include/nob.h"
 
 #define BUILD_FOLDER "./build/"
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    
-
+    // for (int i = 0; i < argc; i++) {
+    //     printf("%d = %s\n", i, argv[i]);
+    // }
     shift(argv, argc);
     Cmd cmd = {0};
     cmd_append(&cmd, "cc", "-Wall", "-Wextra");
@@ -25,11 +26,27 @@ int main(int argc, char** argv)
     if (!cmd_run(&cmd)) return 1;
     cmd.count = 0;
 
+    cmd_append(&cmd, "cc", "-Wall", "-Wextra");
+    cmd_append(&cmd, "-o", BUILD_FOLDER"idoc", "idoc.c");
+    if (!cmd_run(&cmd)) return 1;
+
+    cmd.count = 0;
     if (argc > 0) {
         char* flag = argv[0];
-        if (strcmp(flag, "run") == 0) {
+        shift(argv, argc);
+        if (strcmp(flag, "mp") == 0) {
             cmd_append(&cmd, BUILD_FOLDER"music_player");
+            da_append_many(&cmd, argv, argc);
             if (!cmd_run(&cmd)) return 1;
+        }
+        else if (strcmp(flag, "idoc") == 0) {
+            cmd_append(&cmd, BUILD_FOLDER"idoc");
+            da_append_many(&cmd, argv, argc);
+            if (!cmd_run(&cmd)) return 1;
+        }
+        else {
+            nob_log(NOB_ERROR, "Unknown command %s", flag);
+            exit(1);
         }
     }
 
