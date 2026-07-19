7519be6b2f6ff132069327441de4091a282032ca
thendguy <livm04@hotmail.com>
2026-07-19 16:15:22 +0100

    Finished the lexer

--- idoc/idoc.c
+++ idoc/idoc.c
@@ -7,10 +7,10 @@
 #define COMMENT_CHAR '#'
 
 typedef enum {
-    UNSET = -1,
-    NO_INDENT,
-    TAB = '\t',
-    SPACE = ' ',
+    WS_UNSET = -1,
+    WS_NO_INDENT,
+    WS_TAB = '\t',
+    WS_SPACE = ' ',
 } whitespace_type;
 
 typedef enum {
@@ -27,28 +27,30 @@ typedef enum {
     TOKEN_EQUAL,
     TOKEN_INDENT,
     TOKEN_UNINDENT,
+    TOKEN_NL,
     TOKEN_UNDEFINED,
 } Token_Type;
 
 const char *token_by_name(Token_Type type)
 {
     switch (type) {
-    case TOKEN_EOF:    return "TOKEN_EOF";
-    case TOKEN_VAR:    return "TOKEN_VAR";
-    case TOKEN_STR:    return "TOKEN_STR";
-    case TOKEN_INT:    return "TOKEN_INT";
-    case TOKEN_DOT:    return "TOKEN_DOT";
-    case TOKEN_COLON:  return "TOKEN_COLON";
-    case TOKEN_FLOAT:  return "TOKEN_FLOAT";
-    case TOKEN_OPAREN: return "TOKEN_OPAREN";
-    case TOKEN_CPAREN: return "TOKEN_CPAREN";
-    case TOKEN_COMMA:  return "TOKEN_COMMA";
-    case TOKEN_EQUAL:  return "TOKEN_EQUAL";
-    case TOKEN_INDENT: return "TOKEN_INDENT";
-    case TOKEN_UNINDENT: return "TOKEN_UNINDENT";
+    case TOKEN_EOF:       return "TOKEN_EOF";
+    case TOKEN_VAR:       return "TOKEN_VAR";
+    case TOKEN_STR:       return "TOKEN_STR";
+    case TOKEN_INT:       return "TOKEN_INT";
+    case TOKEN_DOT:       return "TOKEN_DOT";
+    case TOKEN_COLON:     return "TOKEN_COLON";
+    case TOKEN_FLOAT:     return "TOKEN_FLOAT";
+    case TOKEN_OPAREN:    return "TOKEN_OPAREN";
+    case TOKEN_CPAREN:    return "TOKEN_CPAREN";
+    case TOKEN_COMMA:     return "TOKEN_COMMA";
+    case TOKEN_EQUAL:     return "TOKEN_EQUAL";
+    case TOKEN_INDENT:    return "TOKEN_INDENT";
+    case TOKEN_UNINDENT:  return "TOKEN_UNINDENT";
+    case TOKEN_NL:        return "TOKEN_NL";
     case TOKEN_UNDEFINED: return "TOKEN_UNDEFINED";
 
-    default:           return "UNKNOWN";
+    default:              return "UNKNOWN";
     }
 }
 
@@ -65,7 +67,7 @@ typedef struct {
 
 typedef struct {
     whitespace_type type;
-    int count;
+    size_t count;
 } Indent;
 
 typedef struct {
@@ -80,6 +82,8 @@ typedef struct {
     char* file;
     Indent exp_ind;
 
+    Indent last_ind;
+
     Tokens tkns;
 } Lexer;
 
@@ -92,6 +96,7 @@ void error(char* file_name, int line, int col, const char *fmt, ...)
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
+    exit(1);
 }
 
 Lexer lex_init(char* file_name){
@@ -102,7 +107,7 @@ Lexer lex_init(char* file_name){
       .line = 0,
       .sv = sv,
       .file = file_name,
-      .exp_ind = (Indent){UNSET, 0}
+      .exp_ind = (Indent){WS_UNSET, 0}
     };
 }
 
@@ -110,16 +115,58 @@ void strip_comment(String_View *sv) {
     *sv = sv_chop_by_delim(sv, COMMENT_CHAR);
 }
 
-
+bool lexer_check_whitespace(Lexer *l, whitespace_type ws, size_t length){
+    if (l->exp_ind.type != WS_UNSET){
+        if (l->exp_ind.type != ws || length % l->exp_ind.count != 0) {
+            return false;
+        } else return true;
+    } else {
+        l->exp_ind = (Indent){.type = ws, .count = length};
+        return true;
+    }
+}
 
 Token lexer_next_token(Lexer *l){
-    // TODO:  Add indentation count support
+    if (l->sv.count == 0) {
+        return (Token){.type = TOKEN_EOF, .sv = sv_from_cstr("EOF")};
+    }
     size_t length = 0;
     char c = l->sv.data[0];
-    while (isspace(c)) {
-        sv_chop_left(&l->sv, 1); // chop them spaces
-        c = l->sv.data[0];
+    whitespace_type ws;
+    while (true) { // Whitespace handling
+        if (c == ' ') {
+            ws = WS_SPACE;
+            sv_chop_left(&l->sv, 1);
+            length++;
+            c = l->sv.data[0];
+        } else if (c == '\t'){
+            ws = WS_TAB;
+            sv_chop_left(&l->sv, 1);
+            length++;
+            c = l->sv.data[0];
+        } else if (c == '\r'){ // windows carriage return..
+            sv_chop_left(&l->sv, 1);
+            c = l->sv.data[0];
+        } else break;
     }
+    if (length > 0) {
+        if (lexer_check_whitespace(l, ws, length)) {
+            if (l->last_ind.count < length) {
+                l->last_ind = (Indent){.type = ws, .count = length};
+                return (Token){
+                    .type = TOKEN_INDENT,
+                    .sv = sv_from_cstr("->")
+                };
+            } else if (l->last_ind.count > length) {
+                l->last_ind = (Indent){.type = ws, .count = length};
+                return (Token){
+                    .type = TOKEN_UNINDENT,
+                    .sv = sv_from_cstr("<-")
+                };
+            }
+        }
+    }
+    length = 0;
     const char* start = l->sv.data;
     if (isdigit(c)) {
         Token_Type tt = TOKEN_INT;
@@ -160,7 +207,52 @@ Token lexer_next_token(Lexer *l){
     }
     else if (c == '\n') {
         sv_chop_left(&l->sv, 1);
-        return (Token){.type = TOKEN_INDENT, .sv = sv_from_cstr("\n")};
+        return (Token){.type = TOKEN_NL, .sv = sv_from_cstr("\\n")};
+    }
+    else if (c == ':') {
+        sv_chop_left(&l->sv, 1);
+        return (Token){.type = TOKEN_COLON, .sv = sv_from_cstr(":")};
+    }
+    else if (c == '(') {
+        sv_chop_left(&l->sv, 1);
+        return (Token){.type = TOKEN_OPAREN, .sv = sv_from_cstr("(")};
+    }
+    else if (c == ')') {
+        sv_chop_left(&l->sv, 1);
+        return (Token){.type = TOKEN_CPAREN, .sv = sv_from_cstr(")")};
+    }
+    else if (c == '"') {
+        sv_chop_left(&l->sv, 1);
+        length++;
+        c = l->sv.data[0];
+        while (c != '"'){
+            sv_chop_left(&l->sv, 1);
+            if (l->sv.count == 0) {
+                error(l->file, l->line, length, "Unbalanced '\"'.");
+            }
+            length++;
+            c = l->sv.data[0];
+        }
+        sv_chop_left(&l->sv, 1);
+        length++;
+        String_View str = {
+            .data = start,
+            .count = length,
+        };
+        return (Token){.type = TOKEN_STR, .sv = str };
+
+    }
+    else if (c == COMMENT_CHAR){
+        while (true){
+            if (l->sv.count == 0) {
+                return (Token){.type = TOKEN_EOF, .sv = sv_from_cstr("EOF")};
+            }
+            sv_chop_left(&l->sv, 1);
+            c = l->sv.data[0];
+            if (c == '\n') {
+                return (Token){.type = TOKEN_NL, .sv = sv_from_cstr("\\n")};
+            }
+        }
     }
     else {
         String_View sv = {.data = &l->sv.data[0], .count = 1};
@@ -168,106 +260,6 @@ Token lexer_next_token(Lexer *l){
         return (Token){.type = TOKEN_UNDEFINED, .sv = sv};
     }
 }
-
-/* Line lexer_next_line(Lexer *l){ */
-/*     Line line = {0}; */
-/*     line.sv = sv_chop_by_delim(&l->data, '\n'); */
-/*     sv_chop_suffix(&line.sv, sv_from_cstr("\r")); */
-/*     strip_comment(&line.sv); */
-/*     line.ind = (Indent){.type = NO_INDENT, .count = 0}; */
-/*     if (line.sv.count == 0) { */
-/*         l->line++; */
-/*         return line; */
-/*     } */
-/*     char c = line.sv.data[0]; */
-/*     while (c == TAB || c == SPACE) { */
-/*         if (line.ind.type == NO_INDENT){ */
-/*             line.ind.type = c; */
-/*         } else if (l->exp_ind.type != UNSET && l->exp_ind.type != c) { */
-/*             error(l->file, l->line, line.ind.count, */
-/*                   "Inconsistent indentation is not suported."  */
-/*                   "Use only spaces or only tabs"); */
-/*             exit(1); */
-/*         } */
-/*         c = line.sv.data[++line.ind.count]; */
-/*     } */
-/*     if (l->exp_ind.type == UNSET && line.ind.type != NO_INDENT) { */
-/*         l->exp_ind = line.ind; */
-/*     } */
-/*     if (l->exp_ind.type != UNSET && line.ind.count % l->exp_ind.count != 0) { */
-/*         error(l->file, l->line, line.ind.count, "Wrong indentation level"); */
-/*         exit(1); */
-/*     } */
-/*     sv_chop_left(&line.sv, line.ind.count); */
-/*     line.level = (int) ((float)line.ind.count / l->exp_ind.count); */
-/*     l->line++; */
-/*     return line; */
-/* } */
-void parse_line(Line *line, Tokens *tkns){
-    for (size_t i = 0; i < line->sv.count; i++) {
-        char c = line->sv.data[i];
-        if (isdigit(c)) {
-            int start = i;
-            Token_Type tt = TOKEN_INT;
-            while (i < line->sv.count) {
-                char c2 = line->sv.data[i];
-                    if (isdigit(c2)) {
-                        i++;
-                        continue;
-                    } else if (c2 == '.') {
-                        i++;
-                        tt = TOKEN_FLOAT;
-                    } else {
-                        break;
-                    }
-            }
-            String_View number = {
-                .data = line->sv.data + start,
-                .count = i - start,
-            };
-            Token n = {.type = tt, .sv = number};
-            da_append(tkns, n);
-        }
-        else if (isspace(c)){
-            continue;
-        }
-        else if (isalpha(c)){
-            int start = i;
-            while (i < line->sv.count) {
-                char c2 = line->sv.data[i];
-                if (isalpha(c2) || isdigit(c2)) {
-                        i++;
-                    } else {
-                        break;
-                    }
-            }
-            String_View variable = {
-                .data = line->sv.data + start,
-                .count = i - start,
-            };
-            Token s = {.type = TOKEN_VAR, .sv = variable };
-            da_append(tkns, s);
-        }
-        else if (c == '='){
-            String_View eq = {
-                .data = line->sv.data + i,
-                .count = 1,
-            };
-            Token s = {.type = TOKEN_EQUAL, .sv = eq };
-            da_append(tkns, s);
-
-        }
-        else {}
-    }
-}
-
-/* void parse_lines(Lexer *l, Tokens *tkns) { */
-
-/*     while (l->sv.count > 0) { */
-/*         Line line = lexer_next_line(l); */
-/*         parse_line(&line, tkns); */
-/*     } */
-/* } */
 
 Line __debug_line(char* text){
     Line line = (Line){
@@ -277,16 +269,37 @@ Line __debug_line(char* text){
     };
     return line;
 }
+void dump_tokens(Lexer *l){
+    Token t;
+    do {
+        t = lexer_next_token(l);
+        printf(SV_Fmt" => %s\n", SV_Arg(t.sv), token_by_name(t.type));
+    } while (t.type != TOKEN_EOF);
+}
+
+void parse_tokens(Lexer *l) {
+    Token t;
+    while (t.type != TOKEN_EOF){
+        t = lexer_next_token(l);
+        switch (t.type) {
+        case TOKEN_INT: {
+            printf("Parsed an int\n");
+        } break;
+        case TOKEN_VAR: {
+            printf("Parsed a var\n");
+        } break;
+        default:
+            error(__FILE__, __LINE__, 0,
+                  "Unhandled token: %s", token_by_name(t.type));
+        }
+    }
+}
 
 int main(int argc, char** argv){
     (void) argc;
     (void) argv;
-    Tokens tkns = {0};
     Lexer l = lex_init("./idoc/tests/test.idoc");
-    while (l.sv.count > 0){
-        Token t = lexer_next_token(&l);
-        printf(SV_Fmt" => %s\n", SV_Arg(t.sv), token_by_name(t.type));
-    }
+    parse_tokens(&l);
 
     return 0;
 
