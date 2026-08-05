35a7a8eaf86b1f15a625217b4b2104754812be1b
Endy <livm04@hotmail.com>
2026-08-05 17:38:55 +0100

    Started adding actual parser support

--- idoc/idoc.c
+++ idoc/idoc.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define NOB_IMPLEMENTATION
 #include "../include/nob.h"
@@ -31,25 +31,40 @@ typedef enum {
     TOKEN_UNDEFINED,
 } Token_Type;
 
-const char *token_by_name(Token_Type type)
-{
+const char *token_by_name(Token_Type type) {
     switch (type) {
-    case TOKEN_EOF:       return "TOKEN_EOF";
-    case TOKEN_VAR:       return "TOKEN_VAR";
-    case TOKEN_STR:       return "TOKEN_STR";
-    case TOKEN_INT:       return "TOKEN_INT";
-    case TOKEN_DOT:       return "TOKEN_DOT";
-    case TOKEN_COLON:     return "TOKEN_COLON";
-    case TOKEN_FLOAT:     return "TOKEN_FLOAT";
-    case TOKEN_OPAREN:    return "TOKEN_OPAREN";
-    case TOKEN_CPAREN:    return "TOKEN_CPAREN";
-    case TOKEN_COMMA:     return "TOKEN_COMMA";
-    case TOKEN_EQUAL:     return "TOKEN_EQUAL";
-    case TOKEN_INDENT:    return "TOKEN_INDENT";
-    case TOKEN_UNINDENT:  return "TOKEN_UNINDENT";
-    case TOKEN_NL:        return "TOKEN_NL";
-    case TOKEN_UNDEFINED: return "TOKEN_UNDEFINED";
-    default:              return "UNKNOWN";
+    case TOKEN_EOF:
+        return "TOKEN_EOF";
+    case TOKEN_VAR:
+        return "TOKEN_VAR";
+    case TOKEN_STR:
+        return "TOKEN_STR";
+    case TOKEN_INT:
+        return "TOKEN_INT";
+    case TOKEN_DOT:
+        return "TOKEN_DOT";
+    case TOKEN_COLON:
+        return "TOKEN_COLON";
+    case TOKEN_FLOAT:
+        return "TOKEN_FLOAT";
+    case TOKEN_OPAREN:
+        return "TOKEN_OPAREN";
+    case TOKEN_CPAREN:
+        return "TOKEN_CPAREN";
+    case TOKEN_COMMA:
+        return "TOKEN_COMMA";
+    case TOKEN_EQUAL:
+        return "TOKEN_EQUAL";
+    case TOKEN_INDENT:
+        return "TOKEN_INDENT";
+    case TOKEN_UNINDENT:
+        return "TOKEN_UNINDENT";
+    case TOKEN_NL:
+        return "TOKEN_NL";
+    case TOKEN_UNDEFINED:
+        return "TOKEN_UNDEFINED";
+    default:
+        return "UNKNOWN";
     }
 }
 
@@ -59,76 +74,75 @@ typedef struct {
 } Token;
 
 typedef struct {
-    Token *items;
-    size_t count;
-    size_t capacity;
-} Tokens;
-
-typedef struct {
     whitespace_type type;
     size_t count;
 } Indent;
 
 typedef struct {
-    Indent ind;
-    String_View sv;
-    int level;
-} Line;
-
-typedef struct {
     int line;
+    int col;
     String_View sv;
-    char* file;
+    char *file;
     bool at_start;
     Indent exp_ind;
 
     Indent last_ind;
-
-    Tokens tkns;
 } Lexer;
 
+typedef struct Node {
+    String_View key;
+    String_View value;
 
-void error(char* file_name, int line, int col, const char *fmt, ...)
-{
+    struct Node *children;
+    size_t count;
+    size_t capacity;
+} Node;
+
+typedef struct {
+    Lexer lexer;
+    Token current;
+} Parser;
+
+void error(char *file_name, int line, int col, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    nob_log(NOB_ERROR, "%s:%d:%d: ", file_name, line, col);
+    //nob_log(NOB_ERROR, "%s:%d:%d: test ", file_name, line, col);
+    fprintf(stderr, "%s:%d:%d: ", file_name, line, col);
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
     exit(1);
 }
 
-Lexer lex_init(char* file_name){
+Lexer lexer_init(char *file_name) {
     String_Builder sb = {0};
-    read_entire_file(file_name , &sb);
+    read_entire_file(file_name, &sb);
     String_View sv = sb_to_sv(sb);
-    return (Lexer){
-      .line = 0,
-      .sv = sv,
-      .file = file_name,
-      .exp_ind = (Indent){WS_UNSET, 0},
-      .at_start = true
-    };
+    return (Lexer){.line = 1,
+                   .col = 1,
+                   .sv = sv,
+                   .file = file_name,
+                   .exp_ind = (Indent){WS_UNSET, 0},
+                   .at_start = true};
 }
 
-
-bool lexer_check_whitespace(Lexer *l, whitespace_type ws, size_t length){
-    if (!l->at_start){
+bool lexer_check_whitespace(Lexer *l, whitespace_type ws, size_t length) {
+    if (!l->at_start) {
         return false;
-        }
+    }
     l->at_start = false;
-    if (l->exp_ind.type != WS_UNSET){
+    if (l->exp_ind.type != WS_UNSET) {
         if (l->exp_ind.type != ws || length % l->exp_ind.count != 0) {
             return false;
-        } else return true;
+        } else
+            return true;
     } else {
         l->exp_ind = (Indent){.type = ws, .count = length};
         return true;
     }
 }
 
-Token lexer_next_token(Lexer *l){
+Token lexer_next_token(Lexer *l) {
     if (l->sv.count == 0) {
         return (Token){.type = TOKEN_EOF, .sv = sv_from_cstr("EOF")};
     }
@@ -323,19 +337,29 @@ void parse_tokens(Lexer *l) {
             printf("Parsed a var\n");
         } break;
         default:
-            error(__FILE__, __LINE__, 0,
-                  "Unhandled token: %s", token_by_name(t.type));
+            error(__FILE__, __LINE__, 0, "Unhandled token: %s",
+                  token_by_name(t.type));
         }
     }
 }
 
-int main(int argc, char** argv){
-    (void) argc;
-    (void) argv;
-    Lexer l = lex_init("./idoc/tests/test.idoc");
-    //dump_tokens(&l);
-    //parse_tokens(&l);
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    Parser p = parser_init("./idoc/tests/test.idoc");
+    //Parser p = parser_init(argv[1]);
+    print_token(p.current);
+    printf("col = %d\n", p.lexer.col);
+    for (int i = 0; i < 6; i++) {
+        print_token(parser_consume(&p));
+        printf("col = %d\n", p.lexer.col);
+    };
+    parser_expect(&p, TOKEN_VAR);
+    exit(0);
+    const char *t = token_by_name(parser_consume(&p).type);
+    printf("t = %s", t);
+    // parse_tokens(&l);
 
     return 0;
-
 }
